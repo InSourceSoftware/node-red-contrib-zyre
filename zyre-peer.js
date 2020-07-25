2000dcacce535104382e6ce41849389942ec069c
Stephen Riesenberg <sjohnr@hotmail.com>
2020-07-25 11:16:44 -0500

    Reverted zyre peer caching as it does not work properly

--- zyre-peer.js
+++ zyre-peer.js
@@ -2,71 +2,59 @@ module.exports = function(RED) {
   let Zyre = require('zyre.js')
 
   let names = (process.env.ZYRE_PEER_NAMES || '').split(',').map(s => s.trim()).filter(s => s !== '')
-  let peers = {}
+  let events = [
+    'connect',
+    'disconnect',
+    'expired',
+    'whisper',
+    'shout',
+    'join',
+    'leave'
+  ]
 
   function ZyrePeerNode(config) {
     RED.nodes.createNode(this, config)
     this.name = config.name
-    this.port = Number.parseInt(config.port)
-    this.evasive = Number.parseInt(config.evasive)
-    this.expired = Number.parseInt(config.expired)
-    this.interval = Number.parseInt(config.interval)
+    this.port = config.port
+    this.evasive = config.evasive
+    this.expired = config.expired
+    this.interval = config.interval
     this.groups = (config.groups || '').split(',').map(s => s.trim()).filter(s => s !== '')
     this.headers = JSON.parse(config.headers || '{}')
-    this.encoding = config.encoding || 'utf8'
+    this.encoding = config.encoding
 
-    let startZyrePeer = () => {
-      this.zyre = new Zyre({
-        name: names.length > 0 ? names[Math.floor(Math.random() * names.length)] : this.name,
-        headers: this.headers,
-        evasive: this.evasive,
-        expired: this.expired,
-        port: 49152,
-        bport: this.port,
-        binterval: this.interval,
-      })
-      if (this.encoding) {
-        this.zyre.setEncoding(this.encoding)
-      }
+    this.zyre = new Zyre({
+      name: names.length > 0 ? names[Math.floor(Math.random() * names.length)] : this.name,
+      headers: this.headers,
+      evasive: this.evasive,
+      expired: this.expired,
+      port: 49152,
+      bport: this.port,
+      binterval: this.interval,
+    })
+    if (this.encoding) {
+      this.zyre.setEncoding(this.encoding)
+    }
 
-      this.on('close', (removed, done) => {
-        if (removed) {
-         stopZyrePeer().then(() => done && done())
-        } else if (done) {
-          done()
+    let onClose = (removed, done) => {
+      events.forEach(event => this.zyre.removeAllListeners(event))
+
+      this.zyre.stop(() => {
+        if (this.groups) {
+          this.groups.forEach(group => this.zyre.leave(group))
         }
+        this.log(`Stopped zyre peer: ${this.name}`)
       })
-
-      return this.zyre.start()
-        .then(() => this.groups.forEach(group => this.zyre.join(group)))
-        .then(() => this.log(`Started zyre peer: ${this.name}`))
-        // Cache Zyre instance in global context
-        .then(() => peers[this.name] = this.zyre)
+        .then(() => done && done())
     }
 
-    let stopZyrePeer = () => {
-      this.groups.forEach(group => this.zyre.leave(group))
-      return this.zyre.stop(() => this.log(`Stopped zyre peer: ${this.name}`))
-        .then(() => delete peers[this.name])
-    }
-
-    // Check for cached instance
-    this.zyre = peers[this.name]
-    if (!this.zyre) {
-      // Initialize new zyre peer
-      startZyrePeer()
-    } else if (this.name !== this.zyre._name
-        || this.port !== this.zyre._bport
-        || this.evasive !== this.zyre._evasive
-        || this.interval !== this.zyre._binterval
-        || this.groups.length !== this.zyre._zyreNode._groups.length
-        || this.groups.some(group => this.zyre._zyreNode._groups.indexOf(group) < 0)
-        || Object.keys(this.headers).length !== Object.keys(this.zyre._headers || {}).length
-        || Object.keys(this.headers).some(key => this.headers[key] !== this.zyre._headers[key])
-        || this.encoding !== this.zyre._encoding) {
-      // Destroy old zyre peer before initializing new
-      stopZyrePeer().then(() => startZyrePeer())
-    }
+    this.zyre.start(() => {
+      if (this.groups) {
+        this.groups.forEach(group => this.zyre.join(group))
+      }
+      this.log(`Started zyre peer: ${this.name}`)
+    })
+      .then(() => this.on('close', onClose))
   }
 
   RED.nodes.registerType('zyre-peer', ZyrePeerNode)
